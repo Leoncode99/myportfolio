89667f71a1128d49b1632bcb0c5bce7a640af289
Leoncode99 <126984471+Leoncode99@users.noreply.github.com>
2023-06-14 19:12:01 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*cdnjs  search - typed and copy in html source link*/
+
+var typed = new Typed(".typing",{
+    strings:["веб дизайнером","веб разработчиком","проффесионалом своего дела"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
