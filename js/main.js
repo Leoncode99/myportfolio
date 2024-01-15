239b88b54d7514f20c3b16510b664db5ac5b4c5f
Leoncode99 <126984471+Leoncode99@users.noreply.github.com>
2024-01-16 00:34:47 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
 /*cdnjs  search - typed and copy in html source link*/
 
-var typed = new Typed(".typing",{
-    strings:["веб дизайнером","веб разработчиком","проффесионалом своего дела"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
+var typed = new Typed(".typing", {
+  strings: ["дизайнером", "разработчиком сайтов", ""],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
 
 /*modal*/
 
 // modal variables
-const modal = document.querySelector('[data-modal]');
-const modalCloseBtn = document.querySelector('[data-modal-close]');
-const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
+const modal = document.querySelector("[data-modal]");
+const modalCloseBtn = document.querySelector("[data-modal-close]");
+const modalCloseOverlay = document.querySelector("[data-modal-overlay]");
 
 // modal function
-const modalCloseFunc = function () { modal.classList.add('closed') }
+const modalCloseFunc = function () {
+  modal.classList.add("closed");
+};
 
 // modal eventListener
-modalCloseOverlay.addEventListener('click', modalCloseFunc);
-modalCloseBtn.addEventListener('click', modalCloseFunc);
+modalCloseOverlay.addEventListener("click", modalCloseFunc);
+modalCloseBtn.addEventListener("click", modalCloseFunc);
 
 /*modal end*/
